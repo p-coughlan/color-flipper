8a35912e45148a231441a31235da11c299148983
p-coughlan <patrickcoughlan@yahoo.co.uk>
2024-06-02 12:39:32 +0100

    add basic js setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    //get random number between 0 - 3
+    const randonNumber = 1;
+    document.body.style.backgroundColor = colors[randonNumber];
+    color.textContent = colors[randonNumber];
+});
