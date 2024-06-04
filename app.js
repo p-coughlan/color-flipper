102bf9d324797042435ebc393258a28699323d6b
p-coughlan <patrickcoughlan@yahoo.co.uk>
2024-06-04 18:58:05 +0100

    add js for simple operation on an array

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+//colors are an array
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
     //get random number between 0 - 3
-    const randonNumber = 1;
-    document.body.style.backgroundColor = colors[randonNumber];
-    color.textContent = colors[randonNumber];
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
