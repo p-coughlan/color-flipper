1d743ef9a446f71f90db9dee96778315a5bab5f3
p-coughlan <patrickcoughlan@yahoo.co.uk>
2024-06-08 11:06:19 +0100

    add basic js setup for hex color random selection

--- hex.js
+++ hex.js
@@ -1,1 +1,10 @@
+// variable 'hex' is an array of all the hex values, which are used to generate a random hex color code. Containing all the values from 0 to 9 and A to F (numbers and strings both are included in the array)
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// #f15025
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+
+});
